f474db254b6a4628af39cd2325ce5b014afdd4e8
cupangjantan <evaahard@gmail.com>
2026-06-01 18:42:20 +0700

    feat: membuat sistem untuk dashboard (home)

---
+++ routes/dashboard.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, redirect, request, url_for, session
+import config.config as config
+import hashlib
+
+dash = Blueprint('dashboard', __name__)
+
+@dash.route('/home/')
+def home():
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return redirect(url_for('auth.login'))
